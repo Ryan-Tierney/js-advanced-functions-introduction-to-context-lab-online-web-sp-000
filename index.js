c21f3c473871a1db9beee3135041faf7f14cefa8
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-05 21:48:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,11 +38,50 @@ let createTimeOutEvent = function(employee, datestamp) {
   return employee
 } 
 
-let hoursWorkedOnDate = function(employee, soughtDate) { 
-} 
+let hoursWorkedOnDate = function(employee, soughtDate){
+    let inEvent = employee.timeInEvents.find(function(e){
+        return e.date === soughtDate
+    })
 
-let wagesEarnedOnDate = function(employee, soughtDate, hoursWorkedOnDate) { 
+    let outEvent = employee.timeOutEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
 }
 
-let allWagesFor = function(employee) { 
+let wagesEarnedOnDate = function(employee, dateSought){
+    let rawWage = hoursWorkedOnDate(employee, dateSought)
+        * employee.payPerHour
+    return parseFloat(rawWage.toString())
+}
+
+let allWagesFor = function(employee){
+    let eligibleDates = employee.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate(employee, d)
+    }, 0)
+
+    return payable
+}
+
+let createEmployeeRecords = function(src) {
+  return src.map(function(row){
+    return createEmployeeRecord(row)
+  })
+}
+
+let findEmployeebyFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor(rec)
+    }, 0)
 }
