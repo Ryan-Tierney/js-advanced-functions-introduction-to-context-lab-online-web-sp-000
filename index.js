539fac9c6442bfd45c93cf20c116bd3ed7ca76b2
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-05 20:58:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let createEmployeeRecord = function(row) { 
   return {
     firstName: row[0],
-    lastName: row[1],
+    familyName: row[1],
     title: row[2], 
     payPerHour: row[3],
     timeInEvents: [],
