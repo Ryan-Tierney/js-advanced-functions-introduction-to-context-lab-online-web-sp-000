7b04e34ff6a13f8391433725be82a5b9b1d6b299
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-05 21:06:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let createEmployeeRecords = function(employeeRowData) {
 let createTimeinEvent = function(employee, datestamp) { 
   let [date, hour] = datestamp.split(' ')
   
-  employee.timeInEvent
+  employee.timeInEvents.push()
 } 
 
 let createTimeOutEvent = function(employee, datestamp) { 
